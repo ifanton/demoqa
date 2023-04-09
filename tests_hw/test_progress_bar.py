04271ab8fe1a489a441a38d5087ed707c2628c1e
Anton_Panteleev <a.s.panteleev@yandex.ru>
2023-04-09 21:02:09 +0300

    project directory changed and test_progress_bar.py fixed

--- tests_hw/test_progress_bar.py
+++ tests_hw/test_progress_bar.py
@@ -10,22 +10,17 @@ from pages.progress_bar_page import ProgressBarPage
 def test_progress_bar(browser):
     progress_bar_page = ProgressBarPage(browser)
 
-    # progress_bar_page.visit()
-    # assert progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '0'
-    # progress_bar_page.btn_start.click()
-    # while progress_bar_page.progress_bar.get_text() < '51%':
-    #     time.sleep(1)
-    # progress_bar_page.btn_start.click()
-    # assert progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '51'
-
     progress_bar_page.visit()
     time.sleep(2)
     assert progress_bar_page.btn_start.exist()
     assert progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '0'
-    # assert progress_bar_page.progress_bar.check_css('width', '0%')
     progress_bar_page.btn_start.click()
-    while progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') < '51':
-        time.sleep(1)
-    progress_bar_page.btn_start.click()  # не попадает в 51
-    assert progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '51'  # не проходит проверка
+    while True:
+        if progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '51':
+            progress_bar_page.btn_start.click()
+            break
+    # while int(progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow')) < 51:
+    #     print()
+    # progress_bar_page.btn_start.click()
+    assert progress_bar_page.progress_bar.get_dom_attribute('aria-valuenow') == '51'
     time.sleep(3)
